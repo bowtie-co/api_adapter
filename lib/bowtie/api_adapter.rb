8da9a29a249f442b26fdd620972ba9c4fe746ab9
Brandon Cabael <brandon@bowtie.co>
2018-10-15 15:11:17 -0600

    Updated to second point of gem

--- lib/bowtie/api_adapter.rb
+++ lib/bowtie/api_adapter.rb
@@ -2,6 +2,24 @@ require "bowtie/api_adapter/version"
 
 module Bowtie
   module ApiAdapter
-    # Your code goes here...
+    def self.get(path:, params: {}, headers: {})
+      BaseApi.get path: path, params: params, headers: headers
+    end
+
+    def self.post(path:, body: {}, params: {}, headers: {})
+      BaseApi.post path: path, body: body, params: params, headers: headers
+    end
+
+    def self.patch(path:, body: {}, params: {}, headers: {})
+      BaseApi.patch path: path, body: body, params: params, headers: headers
+    end
+
+    def self.put(path:, body: {}, params: {}, headers: {})
+      BaseApi.put path: path, body: body, params: params, headers: headers
+    end
+
+    def self.delete(path:, params: {}, headers: {})
+      BaseApi.delete path: path, params: params, headers: headers
+    end
   end
 end
---
+++ lib/bowtie/api_adapter/base_api.rb
@@ -0,0 +1,141 @@
+# frozen_string_literal: true
+
+require 'json'
+
+##
+# Base API class
+#   - Handles building requests
+#   - Supports defaults for body/params/headers
+##
+module Bowtie
+  class BaseApi
+    # Let instances access these variables
+    attr_accessor :base_url
+    attr_accessor :default_headers
+    attr_accessor :default_params
+    attr_accessor :default_body
+
+    # Define constructor (only "base_url" is required)
+    def initialize(base_url:, default_headers: {}, default_params: {}, default_body: {})
+      # Sanitize base_url before storing in instance var
+      @base_url = sanitize_base_url base_url
+
+      # Set defaults (all default to empty hashes)
+      @default_headers = default_headers
+      @default_params = default_params
+      @default_body = default_body
+    end
+
+    # Execute a GET request for the specified path
+    #   - Optionally provide params/headers
+    #   - params are url parameters (query string)
+    #   - headers are standard HTTP headers
+    def get(path:, params: {}, headers: {})
+      # The options passed to a get request are all "headers", merge incoming with default
+      options = @default_headers.merge(headers)
+
+      # Since all options are passed with headers, add url params under :params symbol
+      options[:params] = @default_params.merge(params)
+
+      # Handle this GET request (returns parsed JSON response body)
+      handle_response RestClient.get(build_url(path), options)
+    end
+
+    # Execute a POST request for the specified path
+    #   - Optionally provide body/params/headers
+    #   - body is the POST request "payload"
+    #   - params are treated as url parameters
+    #   - headers are standard HTTP headers
+    def post(path:, body: {}, params: {}, headers: {})
+      # Merge provided body with any defaults
+      body = @default_body.merge(body)
+
+      # Merge provided headers with any defaults
+      headers = @default_headers.merge(headers)
+
+      body = body.to_json if headers[:content_type] && headers[:content_type].to_s.include?('json')
+
+      # Merge the provided params into headers with any defaults
+      headers[:params] = @default_params.merge(params)
+
+      # Handle this POST request (returns parsed JSON response body)
+      handle_response RestClient.post(build_url(path), body, headers)
+    end
+
+    def patch(path:, body: {}, params: {}, headers: {})
+      # Merge provided body with any defaults
+      body = @default_body.merge(body)
+
+      # Merge provided headers with any defaults
+      headers = @default_headers.merge(headers)
+
+      body = body.to_json if headers[:content_type] && headers[:content_type].to_s.include?('json')
+
+      # Merge the provided params into headers with any defaults
+      headers[:params] = @default_params.merge(params)
+
+      # Handle this POST request (returns parsed JSON response body)
+      handle_response RestClient.patch(build_url(path), body, headers)
+    end
+
+    def put(path:, body: {}, params: {}, headers: {})
+      # Merge provided body with any defaults
+      body = @default_body.merge(body)
+
+      # Merge provided headers with any defaults
+      headers = @default_headers.merge(headers)
+
+      body = body.to_json if headers[:content_type] && headers[:content_type].to_s.include?('json')
+
+      # Merge the provided params into headers with any defaults
+      headers[:params] = @default_params.merge(params)
+
+      # Handle this POST request (returns parsed JSON response body)
+      handle_response RestClient.put(build_url(path), body, headers)
+    end
+
+    def delete(path:, params: {}, headers: {})
+      # The options passed to a get request are all "headers", merge incoming with default
+      options = @default_headers.merge(headers)
+
+      # Since all options are passed with headers, add url params under :params symbol
+      options[:params] = @default_params.merge(params)
+
+      # Handle this GET request (returns parsed JSON response body)
+      handle_response RestClient.delete(build_url(path), options)
+    end
+
+    private
+
+    # Build the full request url
+    #   - Basically just concat @base_url + path
+    #   - Option to insert query string params as part of the request url string
+    def build_url(path, qs = {})
+      # Sanitize provided path
+      path = sanitize_path path
+
+      # Construct and return the full url
+      "#{@base_url}/#{path}?#{qs.to_query}"
+    end
+
+    # Sanitize base_url (remove trailing slash)
+    def sanitize_base_url(base_url)
+      base_url = base_url[0..(base_url.length - 2)] if base_url.end_with? '/'
+
+      base_url
+    end
+
+    # Sanitize path (remove leading slash)
+    def sanitize_path(path)
+      path = path[1..(path.length)] if path.start_with? '/'
+
+      path
+    end
+
+    # Handle generic response
+    def handle_response(response)
+      # Attempt to parse & return response body as JSON
+      response.body ? JSON.parse(response.body) : nil
+    end
+  end
+end
